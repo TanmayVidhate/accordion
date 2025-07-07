7761dae60152e586b16c712e36034986db9f202f
tanmay-vidhate <tanmayvidhate7@gmail.com>
2025-07-07 17:26:09 +0530

    add dummy data

--- src/Components/Accordion.jsx
+++ src/Components/Accordion.jsx
@@ -4,10 +4,10 @@ function Accordion() {
   return (
     <>  
         <div>
-            <p>num</p>
-            <p>title</p>
-            <p>icon</p>
-            <div>text</div>
+            <p>1</p>
+            <p>"What is JSX and why is it used in React?"</p>
+            <p>+</p>
+            <div>JSX (JavaScript XML) is a syntax extension that lets you write HTML inside JavaScript.</div>
         </div>
     </>
   )
