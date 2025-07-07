f17a232ff85d34b3394747500b57b97c307e9fd8
tanmay-vidhate <tanmayvidhate7@gmail.com>
2025-07-07 17:17:49 +0530

    add data.js

---
+++ public/data.js
@@ -0,0 +1,14 @@
+const faq = [
+    {
+        title : "What is JSX and why is it used in React?",
+        ans : "JSX (JavaScript XML) is a syntax extension that lets you write HTML inside JavaScript."
+    },
+    {
+        title : "What is the virtual DOM in React?",
+        ans : "The Virtual DOM is a lightweight copy of the real DOM.React uses it to detect changes efficiently and update only the parts of the UI that changed — improving performance."
+    },
+]
+
+export {
+    faq
+}
