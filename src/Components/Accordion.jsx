9291ca8bf34361bde75ef27a5c7d10c5bc227b0d
tanmay-vidhate <tanmayvidhate7@gmail.com>
2025-07-07 17:18:39 +0530

    add Accordion Component

---
+++ src/Components/Accordion.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Accordion() {
+  return (
+    <>  
+        <div>
+            <p>num</p>
+            <p>title</p>
+            <p>icon</p>
+            <div>text</div>
+        </div>
+    </>
+  )
+}
+
+export default Accordion
