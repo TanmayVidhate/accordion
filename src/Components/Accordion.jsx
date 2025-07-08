dc07c2d7eb05be14019b376abe4fc38c0ff5665a
tanmay-vidhate <tanmayvidhate7@gmail.com>
2025-07-08 11:10:46 +0530

    add logic and Css also

--- src/Components/Accordion.jsx
+++ src/Components/Accordion.jsx
@@ -1,17 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./index.css"
 import Accordionitem from './Accordionitem.jsx'
 import { faqs } from '../../public/data'
 function Accordion() {
+  const [openIndex, setOpenIndex] = useState(null);
+
+  console.log("openIndex ==",openIndex)
   return (
-    <>  
-        <div className='accordion'>
-           {
-            faqs.map((faq,i) => 
-                <Accordionitem faq = {faq} key = {i} number = {i} />
-            )
-           }
-        </div>
+    <>
+      <div className='accordion'>
+        {faqs.map((faq, i) => (
+        
+        <Accordionitem
+          key={i}
+          faqs = {faq}
+          number = {i}
+          isOpen={openIndex === i}
+          onClick={() => { setOpenIndex(openIndex === i ? null : i)}}
+        />
+      ))}
+      </div>
     </>
   )
 }
--- src/Components/Accordionitem.jsx
+++ src/Components/Accordionitem.jsx
@@ -1,20 +1,19 @@
-import React from 'react'
 import "./index.css"
-
-
-function Accordionitem({ faq : {title, ans} ,number}) {
-
+function Accordionitem({faqs : {title ,ans}, number, isOpen, onClick}) {
     return (
         <>
             <div className='item' >
-                <p className='number'>{ number + 1}</p>
+                <p className='number'>{number + 1}</p>
                 <p className='title'>{title}</p>
-                <p className='icon'>+</p>
+                <p className='icon' onClick={onClick}> {isOpen ? "-" : "+"} </p>
                 <p>{" "}</p>
-                <div className='ans'>{ans}</div>
+                <div className={isOpen ? 'content-box' : 'ans'}>{ans}</div>
             </div>
         </>
     )
+
 }
+
+
 
 export default Accordionitem
